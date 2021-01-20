f8d77b24fd973259fe9536b87c80594efcf8bdeb
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-01-20 12:57:52 +0100

    WIP

--- website/components/GraphQL/index.jsx
+++ website/components/GraphQL/index.jsx
@@ -4,20 +4,33 @@ import fetch from "isomorphic-fetch";
 
 import "./styles.css";
 
+const loginMutation = `mutation {
+    tokenCreate(email: "admin@example.com", password: "admin") {
+    token
+  }
+}`
+
 const fetcher = async params => {
-  const data = await fetch("https://demo.saleor.io/graphql/", {
+  const loginData = await fetch("http://localhost:8000/graphql/", {
     method: "post",
     headers: { "content-type": "application/json" },
+    body: JSON.stringify({"query": loginMutation})
+  })
+  const token = (await loginData.json()).data.tokenCreate.token;
+  console.log(token);
+  const data = await fetch("http://localhost:8000/graphql/", {
+    method: "post",
+    headers: { "content-type": "application/json", "Authorization": `JWT ${token}`},
     body: JSON.stringify(params)
   });
   return data.json();
 };
 
-const GraphQL = ({ height = 20, query }) => (
-  <div className="graphql-example" style={{ height: `${height}rem` }}>
-    <GraphiQL fetcher={fetcher} query={query}>
+const GraphQL = ({ height = 20, query, response=undefined, readOnly=false }) => (
+  <div className={readOnly ? "graphql-example read-only" : "graphql-example"} style={{ height: `${height}rem` }}>
+    <GraphiQL response={response} fetcher={fetcher} query={query} readOnly={readOnly} >
       <GraphiQL.Logo>
-        GraphQL Example
+        {readOnly ? "GraphQL Read only example" : "GraphQL Example"}
       </GraphiQL.Logo>
       <GraphiQL.Toolbar />
     </GraphiQL>
